f10c88d88ab2a23532a90292ca77cb2de2155b77
Dima <dm.babii@gmail.com>
2025-06-19 13:53:22 +0100

    ex. 54. create a currency conversion, modify original function

--- workbook/chapter_03/ex_54_helper_currency_conversion.py
+++ workbook/chapter_03/ex_54_helper_currency_conversion.py
@@ -1,7 +1,10 @@
-def compute_usd_total(ammount_in_aud=0, amount_in_gbp=0):
+def convert_currency(amount, rate, margin=0):
+    return amount * rate * (1 + margin)
+
+def compute_usd_total(amount_in_aud=0, amount_in_gbp=0):
     total = 0
-    total += ammount_in_aud * 0.78
-    total += amount_in_gbp * 1.29
+    total += convert_currency(amount_in_aud, 0.78)
+    total += convert_currency(amount_in_gbp, 1.29)
     return total
 
 print(compute_usd_total(amount_in_gbp=10))
