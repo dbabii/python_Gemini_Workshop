9204819397f89856da5912759b6c587d2534180e
Dima <dm.babii@gmail.com>
2025-06-19 16:49:11 +0100

    ex. 54. add a margin

--- workbook/chapter_03/ex_54_helper_currency_conversion.py
+++ workbook/chapter_03/ex_54_helper_currency_conversion.py
@@ -4,7 +4,7 @@ def convert_currency(amount, rate, margin=0):
 def compute_usd_total(amount_in_aud=0, amount_in_gbp=0):
     total = 0
     total += convert_currency(amount_in_aud, 0.78)
-    total += convert_currency(amount_in_gbp, 1.29)
+    total += convert_currency(amount_in_gbp, 1.29, 0.01) #add a 1% margin for conversion
     return total
 
 print(compute_usd_total(amount_in_gbp=10))
