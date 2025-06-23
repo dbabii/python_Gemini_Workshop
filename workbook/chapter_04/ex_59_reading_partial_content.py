6d714d61d2cdba6f2a2c9258071dcfce64d4284b
Dima <dm.babii@gmail.com>
2025-06-23 17:49:57 +0100

    downloaded a txt file for executing first 2 excercises

---
+++ workbook/chapter_04/ex_59_reading_partial_content.py
@@ -0,0 +1,4 @@
+with open('pg37431.txt') as f: # with - close a file automatically after running a code
+    # print(f.read(6)) # read first 6 symbols
+    # if line#2 is uncommented the beginning of next line starts from the 7 symbols
+    print(f.readline())
