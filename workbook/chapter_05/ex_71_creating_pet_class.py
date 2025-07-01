48aa9e02111067f156ecc561e6672594d73fb63e
Dima <dm.babii@gmail.com>
2025-07-01 15:37:16 +0100

    added __init__ method and defined a height attribute

--- workbook/chapter_05/ex_71_creating_pet_class.py
+++ workbook/chapter_05/ex_71_creating_pet_class.py
@@ -3,15 +3,19 @@ class Pet: # define a Pet class
     A class to capture useful information regarding my pets, just incase
     I lose track of them
     """
+    def __init__(self, height):
+        self.height = height
+
     # attributes of the class
     is_human = False
     owner = "Michael Smith"
 
 
 # create an object of class
-chubbles = Pet()
+chubbles = Pet(height=5)
 # check properties for chubbles
 print(chubbles.is_human)
 print(chubbles.owner)
+print(chubbles.height)
 # print docstring
 print(chubbles.__doc__)
