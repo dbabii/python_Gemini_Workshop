91272946bdcf7e158a1ea033ef23a582493f92cd
Dima <dm.babii@gmail.com>
2025-07-01 15:48:28 +0100

    added default color attribute; initiated a new object with default color attribute

--- workbook/chapter_05/ex_72_creating_circle_class.py
+++ workbook/chapter_05/ex_72_creating_circle_class.py
@@ -2,7 +2,7 @@ class Circle:
     # any object of the class will have this attribute
     is_shape = True
     # add __init__ method with instance attributes. They can be different for each objects
-    def __init__(self, radius, color):
+    def __init__(self, radius, color='green'): # change the positional Color argument and add default value
         self.radius = radius
         self.color = color
 
@@ -16,4 +16,8 @@ print(first_circle.color)
 
 print(second_circle.is_shape)
 print(second_circle.radius)
-print(second_circle.color)
+
+my_circle = Circle(5)
+print(my_circle.is_shape)
+print(my_circle.color)
+print(my_circle.radius)
