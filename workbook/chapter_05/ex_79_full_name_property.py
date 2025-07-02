774635cb251d8f466671e542d73a2e83acbb0909
Dima <dm.babii@gmail.com>
2025-07-02 17:52:51 +0100

    ex 79 - added setter and rewrote the object

--- workbook/chapter_05/ex_79_full_name_property.py
+++ workbook/chapter_05/ex_79_full_name_property.py
@@ -7,9 +7,20 @@ class Person():
     def full_name(self):
         return '%s %s' % (self.first_name, self.second_name)
 
+    @full_name.setter
+    def full_name(self, name):
+        first, last = name.split(' ')
+        self.first_name = first
+        self.second_name = last
+
 
 customer = Person('Mary', 'Lou')
+print(customer)
 print(customer.full_name)
 
 # get an error for the following code because there is no used a setter
-customer.full_name = 'Mary Schnidt'
+# change the full name of the customer. Previous data has been gone
+customer.full_name = 'Mary Schmidt'
+print(customer)
+print(customer.full_name)
+print(customer.second_name)
