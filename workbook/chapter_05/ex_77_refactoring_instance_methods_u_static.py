56bdf19c6584ae5d0c9fdf86468481412146df6c
Dima <dm.babii@gmail.com>
2025-07-02 20:23:56 +0100

    ex 83 - overriding methods using super() at ex_77_refactoring_instance_methods_u_static.py

--- workbook/chapter_05/ex_77_refactoring_instance_methods_u_static.py
+++ workbook/chapter_05/ex_77_refactoring_instance_methods_u_static.py
@@ -21,3 +21,23 @@ class Diary:
 my_diary = Diary(datetime.date(2020, 5 , 14), datetime.date(2020,12,25))
 print(my_diary.show_birthday())
 print(my_diary.show_christmas())
+
+class CustomDiary(Diary):
+    def __init__(self, birthday, christmas, date_format):
+        self.date_format = date_format
+# use access to the parent class
+        super().__init__(birthday, christmas)
+# override format date
+    def format_date(self, date):
+        return date.strftime(self.date_format)
+
+first_diary = CustomDiary(datetime.date(1985, 12, 1),
+                          datetime.date(1985, 12, 25),
+                          '%d-%b-%Y')
+
+second_diary = CustomDiary(datetime.date(1958, 12, 1),
+                           datetime.date(1965, 12,25),
+                           '%Y/%m/%d')
+
+print(first_diary.show_birthday())
+print(second_diary.show_birthday())
