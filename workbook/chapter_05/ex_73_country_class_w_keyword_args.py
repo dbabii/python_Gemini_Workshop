88d8293d122619f9e3b84dadf27286ff38a3ca25
Dima <dm.babii@gmail.com>
2025-07-02 15:58:47 +0100

    added additional method to ex_73. played with default parameter

--- workbook/chapter_05/ex_73_country_class_w_keyword_args.py
+++ workbook/chapter_05/ex_73_country_class_w_keyword_args.py
@@ -4,7 +4,15 @@ class Country:
         self.name = name
         self.population = population
         self.size_kmsq = size_kmsq
+# added additional method to convert km to ml
+    def size_miles_sq(self, conversion_rate=0.621371):
+        return self.size_kmsq * conversion_rate ** 2
 
 usa = Country(name='United States of America', size_kmsq=9.8e6) # e6 = 10^6
-
 print(usa.__dict__)
+# create a new object of Country class
+algeria = Country(name='Algeria', size_kmsq=2.382e6)
+print(algeria.size_miles_sq())
+# change the default parameter
+print(algeria.size_miles_sq(conversion_rate=0.6))
+print(algeria.size_miles_sq()) # the default parameter remains unchanged
