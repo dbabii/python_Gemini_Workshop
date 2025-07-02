6b1588366a7f05fe3e8547893608d5cf6b52ae12
Dima <dm.babii@gmail.com>
2025-07-02 18:03:13 +0100

    ex 80 - added validation of temperature limit

--- workbook/chapter_05/ex_80_writing_setter_method.py
+++ workbook/chapter_05/ex_80_writing_setter_method.py
@@ -8,9 +8,15 @@ class Temperature():
 # convert fahrenheits to celsius
     @fahrenheit.setter
     def fahrenheit(self, value):
+# added validation of value
+        if value <= -460:
+            raise ValueError('Temperature less than -460F are not possible')
         self.celsius = (value - 32) * 5 / 9
 
 temp = Temperature(5)
 print(temp.fahrenheit)
 temp.fahrenheit = 32
 print(temp.celsius)
+
+temp.fahrenheit = -460
+print(temp.fahrenheit)
